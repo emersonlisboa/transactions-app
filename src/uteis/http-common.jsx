573237761a4c296062eeae48f606c8cc08281844
emersonlisboa <44339762+emersonlisboa@users.noreply.github.com>
2020-07-28 17:17:41 -0300

    Update http-common.jsx

--- src/uteis/http-common.jsx
+++ src/uteis/http-common.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-    baseURL: 'http://localhost:8090/',
+    baseURL: 'https://mytransaction-api.herokuapp.com/',
     headers: {
         'Content-type': 'application/json',
     },
